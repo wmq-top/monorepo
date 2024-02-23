5b3500350a48b801aa67226921ae3ef21fce2d63
geeker_q <68164309+wmq-top@users.noreply.github.com>
2024-02-23 09:53:31 +0800

    Update dist logic & dts support

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,62 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import Components from 'unplugin-vue-components/vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    dts({ include: './components' }),
-  ],
+    dts({
+      entryRoot: './packages',
+      outputDir: [
+        'dist/types',
+      ],
+    }),
+    Components({
+      dts: 'types/env.d.ts',
+    })],
   build: {
-    minify: true,
+    outDir: 'dist',
+    minify: false,
     cssCodeSplit: false,
-    lib: {
-      formats: ['es'],
-      entry: 'components/index.ts',
-      name: 'components',
-      fileName: format => `index.${format}.js`,
-    },
     rollupOptions: {
-      external: ['vue', 'axios', 'ant-design-vue', '@ant-design/icons-vue', 'ant-design-vue/dist/antd.css'],
-      output: {
-        dir: 'dist',
-        globals: {
-          vue: 'Vue',
+      external: [
+        'vue',
+        'xlsx',
+        'file-saver',
+        'axios',
+        'vue-draggable-next',
+        'ant-design-vue',
+        '@ant-design/icons-vue',
+        'ant-design-vue/dist/antd.css',
+        'shepherd.js',
+        'shepherd.js/dist/css/shepherd.css',
+        'markdown-it',
+        'highlight.js',
+        'clipboard',
+        /^highlight.js\/style/,
+      ],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].mjs',
+          preserveModules: true,
+          exports: 'named',
+          dir: 'dist/es',
         },
-      },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          exports: 'named',
+          dir: 'dist/lib',
+        },
+      ],
+    },
+    lib: {
+      entry: 'packages/index.ts',
     },
   },
   css: {
