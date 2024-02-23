ba5bc58aefc81d3094c3686f3736d5a0600c091e
geeker_q <68164309+wmq-top@users.noreply.github.com>
2024-02-23 10:39:41 +0800

    Create createComponent.ts

---
+++ utils/createComponent.ts
@@ -0,0 +1,62 @@
+import { argv } from 'process'
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import { docDemeComponentStr, generateMatrixStr, generateReadmeStr } from './constants'
+const cwd = process.cwd()
+interface RouterConfigData {
+  groupKey: string
+  groupName: string
+  children: Record<'label' | 'key', string>[]
+}
+
+// 方法2: 使用 ES6 中的解构赋值和 toUpperCase
+function capitalizeFirstLetter2(str) {
+  const [first, ...rest] = str
+  return first.toUpperCase() + rest.join('')
+}
+async function init() {
+  const componentName = argv[2].toLocaleLowerCase()
+  const componentLabel = argv[3]?.toLocaleUpperCase() || componentName.toLocaleUpperCase()
+
+  const routerViewConfigUri = path.join(cwd, './src/views/components-view/constants.json')
+  const routerViewConfigStr = readFileSync(routerViewConfigUri, {
+    encoding: 'utf-8',
+  })
+  const routerViewConfigObj = JSON.parse(routerViewConfigStr)
+  const currentComponentList: string[] = []
+  routerViewConfigObj.forEach((ele: RouterConfigData) => {
+    ele.children.forEach((item) => {
+      currentComponentList.push(item.key)
+    })
+  })
+
+  if (currentComponentList.includes(componentName)) {
+    console.error('当前的组件已存在请更换组件名称')
+    return
+  }
+  routerViewConfigObj[0].children.push({
+    label: componentLabel,
+    key: componentName,
+  })
+  const concatString = JSON.stringify(routerViewConfigObj)
+  writeFileSync(routerViewConfigUri, concatString)
+
+  const componentDirUri = path.join(cwd, `./src/components/${componentName}`)
+  mkdirSync(componentDirUri, {
+    recursive: true,
+  })
+
+  const DemoFileName = `${componentName.split('-').map(i => capitalizeFirstLetter2(i.toLocaleLowerCase())).join('')}Demo`
+
+  writeFileSync(path.join(componentDirUri, `${DemoFileName}.vue`), docDemeComponentStr)
+  writeFileSync(path.join(componentDirUri, 'index.vue'), generateMatrixStr(componentName, DemoFileName))
+  writeFileSync(path.join(componentDirUri, 'README.md'), generateReadmeStr(componentLabel))
+
+  console.log(`${DemoFileName} 创建成功`)
+}
+try {
+  init()
+}
+catch (e) {
+  console.error(e)
+}
